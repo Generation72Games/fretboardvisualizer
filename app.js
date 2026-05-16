4028d68f32e2d79c9940dd12d6411d3f4a0bcb6e
Generation72Games <generationseventytwogames@gmail.com>
2026-05-16 19:58:40 -0400

    Added "Major" for chords

--- app.js
+++ app.js
@@ -125,6 +125,7 @@ const CHORD_QUALITIES = {
     Sus4: [0, 5, 7]
   },
   seventh: {
+    Major: [0, 4, 7],
     Maj7: [0, 4, 7, 11],
     Dom7: [0, 4, 7, 10],
     Min7: [0, 3, 7, 10],
@@ -400,7 +401,7 @@ function init() {
 
   els.chordTypeSelect.addEventListener("change", () => {
     const qualities = Object.keys(CHORD_QUALITIES[els.chordTypeSelect.value]);
-    fillSelect(els.chordSelect, qualities, qualities[0]);
+    fillSelect(els.chordSelect, qualities, qualities.includes("Major") ? "Major" : qualities[0]);
     render();
   });
 
